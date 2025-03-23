532c5c03ba545d337aa2751b474314dfa63eca3c
Jaques Strydom <jaques.strydom@gmail.com>
2025-03-23 12:06:00 +0200

    Login Form and User Authentication
    Add login URL
    Add login view
    Add login template
    Login user when registering

---
+++ Lesson10/myproject/users/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+
+
+# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save())
+            return redirect('posts:list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {"form": form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('posts:list')
+    else:
+        form = AuthenticationForm(request=request)
+    return render(request, 'users/login.html', {"form": form})
